65beab6a9a3770b48b93624e096b236fb5caa12f
Nichbwasy <nichbwasy.jvs@gmail.com>
2022-02-14 09:22:13 +0300

    Commit 1

---
+++ src/main/java/com/site/controlers/AlbumController.java
@@ -0,0 +1,16 @@
+package com.site.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AlbumController {
+    @RequestMapping("/album")
+    public String blog(Model model){
+        model.addAttribute("title", "Album");
+        model.addAttribute("album_header", "Welcome to our album page!");
+        return "album";
+    }
+
+}
